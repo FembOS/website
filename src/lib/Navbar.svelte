a766e63e7c19734a0b617c39da38d566b5fdad11
Octozu <78366921+Octozu@users.noreply.github.com>
2022-05-07 21:59:06 +0200

    🚧 add: Navbar

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -3,9 +3,58 @@
 </script>
 
 <div> 
+    <header>
+      <a href="#" class="logo">Logo</a>
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Discord</a></li>
+        </ul>
+      </header>
+      <section class="banner"></section>
     
 </div>
 
-<style> 
+<style>
+  header{
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 90%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    transition: 0.6s;
+    margin: 40px 100px;
+  }
+  header .logo{
+    position: relative;
+    font-weight: 700;
+    color: black;
+    text-decoration: none;
+    font-size: 2em;
+    text-transform: uppercase;
+    letter-spacing: 2px;
+    transition: 0.6s;
+}
+  header ul{
+    position: relative;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  header ul li{
+    position: relativse;
+    list-style: none;
+  }
+  header ul li a{
+    position: relative;
+    margin: 0 15px;
+    text-decoration: none;
+    color: black;
+    letter-spacing: 2px;
+    font-weight: 500px;
+    transition: 0.6s;
+  }
     
 </style>
